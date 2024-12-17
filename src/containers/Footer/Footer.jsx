95927d4dbec8efceca71a9f3035f911a2721465e
TenpennyUMFPOS <med19aziz@gmail.com>
2024-12-17 09:39:05 +0000

    my initial commit

---
+++ src/containers/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+export function Footer() {
+  return (
+    <footer>
+      <h3>All Content including images and products strictly Copyright © 2020-2024.</h3>
+    </footer>
+  );
+}
+
---
+++ src/containers/Header/Header.jsx
@@ -0,0 +1,8 @@
+export function Header() {
+  return (
+    <header>
+      <h1>LS Forums</h1>
+    </header>
+  );
+}
+
