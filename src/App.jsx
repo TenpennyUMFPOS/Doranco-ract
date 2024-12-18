25e8ab66b9948fdc8707576855fb70672b7afd08
TenpennyUMFPOS <med19aziz@gmail.com>
2024-12-18 13:53:50 +0000

    exercice 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Header } from "./composants/containers/Header/Header";
 import { Footer } from "./composants/containers/Footer/Footer";
 import Routine from "./components/Routine";
+import Menu from "./components/Menu";
 
 function App() {
   return (
     <div>
       <Header />
-      <Routine />
+      <Menu />
       <Footer />
     </div>
   );
