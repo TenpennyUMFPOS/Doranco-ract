61158c2232704229adde43f21332b11f7d28ae08
TenpennyUMFPOS <med19aziz@gmail.com>
2024-12-19 08:32:57 +0000

    exercice 7

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { Header } from "./composants/containers/Header/Header";
 import { Footer } from "./composants/containers/Footer/Footer";
 import Routine from "./components/Routine";
 import Menu from "./components/Menu";
+import Hobbies from "./components/Hobbies";
 
 function App() {
   return (
     <div>
       <Header />
-      <Menu />
+      <Hobbies />
       <Footer />
     </div>
   );
--- src/components/Hobbies.jsx
+++ src/components/Hobbies.jsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Hobbies = () => {
-    const hobbies = ['Football', 'Gaming', 'RRRR'];
+function Hobbies() {
+    const [hobbies, setHobbies] = useState([]);
+    const [newHobby, setNewHobby] = useState('');
+
+    const handleAddHobby = () => {
+        if (newHobby.trim() !== '') {
+            setHobbies((prevHobbies) => [...prevHobbies, newHobby]);
+            setNewHobby('');
+        } else {
+            alert('Le champ ne peut pas être vide.');
+        }
+    };
+
     return (
         <div>
-            <h3>Hobbies:</h3>
+            <h2>Liste des Hobbies</h2>
             <ul>
                 {hobbies.map((hobby, index) => (
                     <li key={index}>{hobby}</li>
                 ))}
             </ul>
+            <input
+                type="text"
+                value={newHobby}
+                onChange={(e) => setNewHobby(e.target.value)}
+                placeholder="Ajouter un hobby"
+            />
+            <button onClick={handleAddHobby}>Ajouter</button>
         </div>
     );
-};
+}
 
 export default Hobbies;
